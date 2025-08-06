e992014710d6f5cede8e96706db065e4baf490bf
yokimklein <commits@yokim.org>
2025-08-06 14:31:11 +0100

    Automatically convert sessionID strings to secure addresses in user_xuid_from_secure_address

--- hf2p_server/source/hf2p/user.cpp
+++ hf2p_server/source/hf2p/user.cpp
@@ -12,32 +12,29 @@ qword user_xuid_from_secure_address(s_transport_secure_address const* secure_add
         return USER_SYSTEM;
     }
 
-    s_transport_secure_address addresses[3];
-    // aed61e55-632d-4ea5-9bf9-ead4322b640f ZZVERTIGO
-    byte temp_address1[16] = { 0x55, 0x1e, 0xd6, 0xae, 0x2d, 0x63, 0xa5, 0x4e, 0x9b, 0xf9, 0xea, 0xd4, 0x32, 0x2b, 0x64, 0x0f };
-    // FFC6472C-AD90-4393-AD53-0741123F29CE ILIKENAME
-    byte temp_address2[16] = { 0x2c, 0x47, 0xc6, 0xff, 0x90, 0xad, 0x93, 0x43, 0xad, 0x53, 0x07, 0x41, 0x12, 0x3f, 0x29, 0xce };
-    // c073068b-5ca5-4c53-84cf-c720dfaa2f40 YOKIM
+    s_userid_sessionid_pair user_info[] =
+    {
+        { 1725920991, L"{11761bb3-39ac-4458-a3a8-c1e8026a5833}" }, // zzVertigo
+        { 1725921312, L"{ecb9a859-da69-4c5d-ad81-59e446fd4342}" }, // Yokim
+    };
+    s_transport_secure_address addresses[NUMBEROF(user_info)];
 
-    // NEW: 0363777e-f9af-46b9-a26e-3c7bf1d42ce4
-    // 0x7e, 0x77, 0x63, 0x03
-    //                               reversed                reversed          reversed         forward                   forward
-    byte temp_address3[16] = { 0x56, 0xf0, 0xea, 0xba, /**/ 0xbd, 0xc2, /**/ 0x76, 0x47, /**/ 0xac, 0xbf, /**/ 0xe8, 0x0c, 0xe0, 0x40, 0xbe, 0x68};
-
-    memcpy(&addresses[0], temp_address1, sizeof(s_transport_secure_address));
-    memcpy(&addresses[1], temp_address2, sizeof(s_transport_secure_address));
-    memcpy(&addresses[2], temp_address3, sizeof(s_transport_secure_address));
-
-    qword user_ids[] = { 1725920991, 1725921323, 1725921312 };
+    for (long i = 0; i < NUMBEROF(addresses); i++)
+    {
+        //RPC_STATUS result = UuidFromStringA((RPC_CSTR)user_info[i].session_id, (UUID*)&addresses[i]);
+        HRESULT result = CLSIDFromString(user_info[i].session_id, (LPCLSID)&addresses[i]);
+        ASSERT(result == S_OK);
+    }
 
     for (long i = 0; i < NUMBEROF(addresses); i++)
     {
         if (addresses[i] == *secure_address)
         {
-            return user_ids[i];
+            return user_info[i].user_id;
         }
     }
 
-    printf("ONLINE/CLIENT/STUB_LOG_PATH,STUB_LOG_FILTER: user_xuid_from_secure_address: could not find userID associated with secure address [%s]!\n", transport_secure_address_get_string(secure_address));
+    printf("ONLINE/CLIENT/STUB_LOG_PATH,STUB_LOG_FILTER: user_xuid_from_secure_address: could not find userID associated with sessionID [%s]!\n",
+        transport_secure_address_get_string(secure_address));
     return USER_INVALID;
 }
--- hf2p_server/source/hf2p/user.h
+++ hf2p_server/source/hf2p/user.h
@@ -6,3 +6,9 @@
 
 struct s_transport_secure_address;
 qword user_xuid_from_secure_address(s_transport_secure_address const* secure_address);
+
+struct s_userid_sessionid_pair
+{
+	qword user_id;
+	const wchar_t* session_id;
+};
